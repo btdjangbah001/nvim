97c5dc9d768d5bc0817a416c6e0ec59c6337f70b
btdjangbah001 <btdjangbah001@st.ug.edu.gh>
2023-05-14 18:09:30 +0000

    added rust-tools and dapui

--- lua/personal/plugins.lua
+++ lua/personal/plugins.lua
@@ -128,15 +128,9 @@ local plugins = {
   -- require 'kickstart.plugins.autoformat',
   -- require 'kickstart.plugins.debug',
 
-  -- NOTE: The import below automatically adds your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  --
-  --    An additional note is that if you only copied in the `init.lua`, you can just comment this line
-  --    to get rid of the warning telling you that there are not plugins in `lua/custom/plugins/`.
-  -- { import = 'custom.plugins' },
+  'simrat39/rust-tools.nvim',
+  'mfussenegger/nvim-dap',
+  'rcarriga/nvim-dap-ui',
 }
 
 local opts = {}
